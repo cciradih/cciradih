8191d9796d03d47ffc43e992f3e893b71b76ce5d
Cciradih <trash@cciradih.eu.org>
2024-05-16 10:34:48 +0800

    初始提交

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
+
